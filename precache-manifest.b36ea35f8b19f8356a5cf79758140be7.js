e945067530ac206aaf1d9832fb6af3e4a6c7599e
Amritpal <amritpal.se@gmail.com>
2021-04-19 12:38:43 +0530

    Updates

---
+++ precache-manifest.b36ea35f8b19f8356a5cf79758140be7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afcc5bb5d28b06899996459c609e3e0c",
+    "url": "/lifecycles/index.html"
+  },
+  {
+    "revision": "1da81361d41e37e042a4",
+    "url": "/lifecycles/static/css/main.c15d3d91.chunk.css"
+  },
+  {
+    "revision": "3d5ac3a15584ce723d19",
+    "url": "/lifecycles/static/js/2.7a00743e.chunk.js"
+  },
+  {
+    "revision": "1da81361d41e37e042a4",
+    "url": "/lifecycles/static/js/main.490d36ef.chunk.js"
+  },
+  {
+    "revision": "62d349ca70935b2cb05d",
+    "url": "/lifecycles/static/js/runtime~main.eb23e07f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lifecycles/static/media/logo.5d5d9eef.svg"
+  }
+]);
